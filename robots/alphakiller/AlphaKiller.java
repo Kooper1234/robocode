ffaf21ac3056664bba3029e2fb1d6e1b75dc3bc5
Kooper Odar <kooprynn@gmail.com>
2024-05-29 23:19:33 -0400

    number 3

--- robots/alphakiller/AlphaKiller.java
+++ robots/alphakiller/AlphaKiller.java
@@ -1,75 +1,71 @@
 package alphakiller;
+
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
-
 
 public class AlphaKiller extends Robot {
 
     double gabeDirection;
-    double gunDirection;
 
-    public void run(){
+    public void run() {
+        // Move to the nearest corner with 50 distance spaces from the walls
+        moveToCorner();
 
+        // Continuous scanning and targeting
+        while (true) {
+            turnGunLeft(360); // Scan 360 degrees
+        }
+    }
+
+    // Method to move the robot to the nearest corner with some distance from the walls
+    private void moveToCorner() {
         double height = getBattleFieldHeight();
         double width = getBattleFieldWidth();
-        double direction = getHeading();
-        gunDirection = getGunHeading();
         double yPos = getY();
         double xPos = getX();
 
-        if (yPos < height/2){
-            turnRight(180-direction);
-            ahead(height+yPos-50);
-            if (xPos < width/2){
-                turnRight(90);
-                ahead(width-xPos-50);
-            }
-            else if (xPos >= width/2){
-                turnLeft(90);
-                ahead(width-xPos-50);
-            }
-        }
-        else if (yPos >= height/2){
-            turnRight(360-direction);
-            ahead(height-yPos-50);
-            if (xPos < width/2){
-                turnLeft(90);
-                ahead(width-xPos-50);
-            }
-            else if (xPos >= width/2){
-                turnRight(90);
-                ahead(width-xPos-50);
-            }
-        }
-        while(true){  
-            boolean inRange = false;
-            if (((gabeDirection-30)<=(gunDirection)) && ((gabeDirection+30)>=(gunDirection)) && (gabeDirection != 0.0)){
-                inRange = true;
-                if (gunDirection <= gabeDirection){
-                    turnLeft(gabeDirection-gunDirection);
-                }
-                else if (gunDirection > gabeDirection){
-                    turnGunRight(gunDirection-gabeDirection);
-                }
+        if (yPos < height / 2) {
+            if (xPos < width / 2) {
+                goToPosition(width - 50, height - 50);
             } else {
-                inRange = false;
+                goToPosition(50, height - 50);
             }
-            while (inRange == true){
-                turnGunLeft(60);
-                turnGunRight(60);
+        } else {
+            if (xPos < width / 2) {
+                goToPosition(width - 50, 50);
+            } else {
+                goToPosition(50, 50);
             }
-             
-            turnGunLeft(360);
-
-
         }
-        
     }
 
-    public void onScannedRobot(ScannedRobotEvent e){
-        fire(1);
-        gabeDirection = gunDirection;
+    // Method to move the robot to a specific position
+    private void goToPosition(double x, double y) {
+        double dx = x - getX();
+        double dy = y - getY();
+        double distance = Math.sqrt(dx * dx + dy * dy);
+        double angle = Math.toDegrees(Math.atan2(dx, dy));
+        turnRight(normalizeBearing(angle - getHeading()));
+        ahead(distance);
+    }
 
-        
+    // Method to normalize bearing between -180 and 180 degrees
+    private double normalizeBearing(double angle) {
+        while (angle > 180) {
+            angle -= 360;
+        }
+        while (angle < -180) {
+            angle += 360;
+        }
+        return angle;
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        // Lock onto the target's direction
+        gabeDirection = getHeading() + e.getBearing();
+        // Turn gun towards the target
+        turnGunRight(normalizeBearing(gabeDirection - getGunHeading()));
+        // Fire at the target
+        fire(2);
     }
 }
